842b9d6602493733c8c9ad0df6ba86fdddc0e65f
MemaroX <boy15maher50@gmail.com>
2025-07-31 09:18:55 +0300

    feat: Implement basic keyboard driver

--- kernel.asm
+++ kernel.asm
@@ -6,6 +6,12 @@ start:
     mov ax, 0x10
     mov ds, ax
 
+    ; Setup IDT
+    lidt [idt_descriptor]
+
+    ; Enable interrupts
+    sti
+
     mov esi, msg_kernel
     mov edi, 0xb8000
     mov ah, 0x0f
@@ -22,5 +28,29 @@ print_loop:
 
 hang:
     jmp $
+
+keyboard_handler:
+    in al, 0x60
+    ; For now, just echo the scancode to the screen
+    ; (This is not a real scancode to ASCII conversion)
+    mov [edi], al
+    inc edi
+    mov byte [edi], 0x0f
+    inc edi
+    iret
+
+idt_start:
+    ; Keyboard interrupt
+    dw keyboard_handler
+    dw 0x08
+    db 0
+    db 0x8e
+    dw 0
+
+idt_end:
+
+idt_descriptor:
+    dw idt_end - idt_start - 1
+    dd idt_start
 
 msg_kernel: db 'Kernel successfully loaded in 32-bit protected mode!', 0
